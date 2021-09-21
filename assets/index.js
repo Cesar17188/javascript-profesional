eed62a402a82f17978b51bf48eec8f4429c29a49
cesareli17188 <cesareli17188@gmail.com>
2021-09-20 20:19:32 -0500

    scopes

--- assets/index.js
+++ assets/index.js
@@ -1,24 +1,8 @@
+import MediaPlayer from "./MediaPlayer.js";
+
 const video = document.querySelector("video");
-const button = document.getElementById("playButton");
-
-function MediaPlayer(config) {
-  this.media = config.el;
-}
-
-MediaPlayer.prototype.play = function () {
-  this.media.play();
-};
-
-MediaPlayer.prototype.pause = function () {
-  this.media.pause();
-};
-
-MediaPlayer.prototype.tooglePlay = function () {
-  this.media.paused ? this.media.play() : this.media.pause();
-};
-
 const player = new MediaPlayer({ el: video });
 
 // button.onclick = () => player.play();
-
+const button = document.getElementById("playButton");
 button.onclick = () => player.tooglePlay();
