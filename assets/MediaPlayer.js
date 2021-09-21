0a8262ee7df105ec1a096178a3b9c61653ab0670
cesareli17188 <cesareli17188@gmail.com>
2021-09-21 17:58:48 -0500

    plugins

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,6 +1,15 @@
 function MediaPlayer(config) {
   this.media = config.el;
+  this.plugins = config.plugins || [];
+
+  this._initPlugins();
 }
+
+MediaPlayer.prototype._initPlugins = function () {
+  this.plugins.forEach((plugin) => {
+    plugin.run(this);
+  });
+};
 
 MediaPlayer.prototype.play = function () {
   this.media.play();
@@ -12,6 +21,18 @@ MediaPlayer.prototype.pause = function () {
 
 MediaPlayer.prototype.tooglePlay = function () {
   this.media.paused ? this.media.play() : this.media.pause();
+};
+
+MediaPlayer.prototype.mute = function () {
+  this.media.muted = true;
+};
+
+MediaPlayer.prototype.unmute = function () {
+  this.media.muted = false;
+};
+
+MediaPlayer.prototype.mute_unmute = function () {
+  this.media.muted ? (this.media.muted = false) : (this.media.muted = true);
 };
 
 export default MediaPlayer;
--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,12 @@
 import MediaPlayer from "./MediaPlayer.js";
+import AutoPlay from "./plugins/AutoPlay.js";
 
 const video = document.querySelector("video");
-const player = new MediaPlayer({ el: video });
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
 
 // button.onclick = () => player.play();
-const button = document.getElementById("playButton");
-button.onclick = () => player.tooglePlay();
+const Playbutton = document.getElementById("playButton");
+Playbutton.onclick = () => player.tooglePlay();
+
+const Mutebutton = document.getElementById("muteButton");
+Mutebutton.onclick = () => player.mute_unmute();
