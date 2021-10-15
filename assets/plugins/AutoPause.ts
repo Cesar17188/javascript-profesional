00b49818bf817065018f50401d92a96ef7e1aeef
cesareli17188 <cesareli17188@gmail.com>
2021-10-15 13:04:53 -0500

    de js a ts

--- assets/plugins/AutoPause.ts
+++ assets/plugins/AutoPause.ts
@@ -1,4 +1,9 @@
+import MediaPlayer from "../MediaPlayer";
+
 class AutoPause {
+  private threshold: number;
+  player: MediaPlayer;
+
   constructor() {
     this.threshold = 0.25;
     this.handleIntersection = this.handleIntersection.bind(this);
@@ -15,15 +20,15 @@ class AutoPause {
     document.addEventListener("visibilitychange", this.handleVisibilityChange);
   }
 
-  handleIntersection(entries) {
+  private handleIntersection(entries: IntersectionObserverEntry[]) {
     const entry = entries[0];
 
-    const isVisible = entry.IntersectionRatio >= this.threshold;
+    const isVisible = entry.intersectionRatio >= this.threshold;
 
     isVisible ? this.player.play() : this.player.pause();
   }
 
-  handleVisibilityChange() {
+  private handleVisibilityChange() {
     const isVisible = document.visibilityState === "visible";
 
     isVisible ? this.player.play() : this.player.pause();
