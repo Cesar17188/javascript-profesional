8238500f21f9c1002093393e0eaeab63eccdd6ad
cesareli17188 <cesareli17188@gmail.com>
2021-10-15 12:51:40 -0500

    interfaces

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -65,3 +65,31 @@ function fullName(firstName: string, lastName: string ="Smith"): string {
 const richard = fullName('Agente');
 console.log(richard);
 
+// Interfaces
+
+interface Rectangulo {
+    ancho: number,
+    alto: number,
+    color?: Color
+};
+
+let rect: Rectangulo = {
+    ancho: 4,
+    alto: 6,
+    color: Color.Rojo
+};
+
+function area(r: Rectangulo) {
+    return r.alto * r.ancho;
+}
+
+const areaRect = area(rect);
+console.log(areaRect);
+
+console.log(rect.toString());
+
+rect.toString = function() {
+    return this.color ? `Un rectangulo ${this.color}` : 'Un Rectangulo';
+}
+
+console.log(rect.toString());
